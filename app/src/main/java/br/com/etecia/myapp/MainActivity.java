9e0c67e00e5acc5111b9f97ce9cced6963aa7fe1
MuriloOGenio <mueugi@gmail.com>
2023-08-29 19:52:49 -0300

    add: adaptador

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -1,10 +1,18 @@
 package br.com.etecia.myapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+    TabLayout tabLayout;
+
+    ViewPager2 viewPager2;
+
+    MyViewPagerAdapter myViewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
